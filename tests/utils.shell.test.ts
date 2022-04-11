bafffaca4dec35d923e04fdadf4e225d49b33066
Tomer Figenblat <tfigenbl@redhat.com>
2022-04-11 18:20:34 +0300

    test: added extra layer of suite for clarification
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>

--- tests/utils.shell.test.ts
+++ tests/utils.shell.test.ts
@@ -7,38 +7,41 @@ import { checkToolExists, executeShellCommand } from '../src/utils/shell';
 
 chaiUse(chaiAsPromised);
 
-suite('Test cases for the checkToolExists utility function', () => {
-	beforeEach(() => sinon.restore());
+suite('Test cases for the shell utility functions', () => {
 
-	test('When checking if an existing tool exists, the promise should be fulfilled', async () => {
-		// @ts-ignore
-		sinon.stub(shell, 'exec').withArgs('command -v existing-tool').returns({code: 0});
-		return expect(checkToolExists('existing-tool')).to.eventually.be.fulfilled;
+	suite('Test cases for the checkToolExists utility function', () => {
+		beforeEach(() => sinon.restore());
+
+		test('When checking if an existing tool exists, the promise should be fulfilled', async () => {
+			// @ts-ignore
+			sinon.stub(shell, 'exec').withArgs('command -v existing-tool').returns({code: 0});
+			return expect(checkToolExists('existing-tool')).to.eventually.be.fulfilled;
+		});
+
+		test('When checking if a non-existing tool exists, the promise should be rejected', async () => {
+			// @ts-ignore
+			sinon.stub(shell, 'exec').withArgs('command -v non-existing-tool').returns({code: 999});
+			return expect(checkToolExists('non-existing-tool')).to.eventually.be.rejected;
+		});
 	});
 
-	test('When checking if a non-existing tool exists, the promise should be rejected', async () => {
-		// @ts-ignore
-		sinon.stub(shell, 'exec').withArgs('command -v non-existing-tool').returns({code: 999});
-		return expect(checkToolExists('non-existing-tool')).to.eventually.be.rejected;
-	});
-});
+	suite('Test cases for the executeShellCommand utility function', () => {
+		beforeEach(() => sinon.restore());
 
-suite('Test cases for the executeShellCommand utility function', () => {
-	beforeEach(() => sinon.restore());
+		test('When executing a successful command, the promise should be fulfilled with the stdout', async () => {
+			let dummyCommand = 'dummy-successful-command';
+			let dummyExecution = {code: 0, stdout: 'all is good in the hood'}; // note stdout and not stderr
+			// @ts-ignore
+			sinon.stub(shell, 'exec').withArgs(dummyCommand).returns(dummyExecution);
+			return expect(executeShellCommand(dummyCommand)).to.eventually.be.fulfilled.and.equal(dummyExecution.stdout);
+		});
 
-	test('When executing a successful command, the promise should be fulfilled with the stdout', async () => {
-		let dummyCommand = 'dummy-successful-command';
-		let dummyExecution = {code: 0, stdout: 'all is good in the hood'}; // note stdout and not stderr
-		// @ts-ignore
-		sinon.stub(shell, 'exec').withArgs(dummyCommand).returns(dummyExecution);
-		return expect(executeShellCommand(dummyCommand)).to.eventually.be.fulfilled.and.equal(dummyExecution.stdout);
-	});
-
-	test('When executing a failed command, the promise should be fulfilled with the stderr', async () => {
-		let dummyCommand = 'dummy-failed-command';
-		let dummyExecution = {code: 999, stderr: 'oh my god they killed kenny'}; // note stderr and not stdout
-		// @ts-ignore
-		sinon.stub(shell, 'exec').withArgs(dummyCommand).returns(dummyExecution);
-		return expect(executeShellCommand(dummyCommand)).to.eventually.be.rejectedWith(dummyExecution.stderr);
+		test('When executing a failed command, the promise should be fulfilled with the stderr', async () => {
+			let dummyCommand = 'dummy-failed-command';
+			let dummyExecution = {code: 999, stderr: 'oh my god they killed kenny'}; // note stderr and not stdout
+			// @ts-ignore
+			sinon.stub(shell, 'exec').withArgs(dummyCommand).returns(dummyExecution);
+			return expect(executeShellCommand(dummyCommand)).to.eventually.be.rejectedWith(dummyExecution.stderr);
+		});
 	});
 });
