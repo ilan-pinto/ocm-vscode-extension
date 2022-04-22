e85e77b7d9000c25d6e6d42f18384114df4cdbe9
Tomer Figenblat <tfigenbl@redhat.com>
2022-04-22 10:53:48 +0300

    test: fix tests failing on windows runner
    Signed-off-by: Tomer Figenblat <tfigenbl@redhat.com>

--- tests/utils.filesystem.test.ts
+++ tests/utils.filesystem.test.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { expect, use as chaiUse } from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 import { beforeEach } from 'mocha';
@@ -6,6 +7,15 @@ import { createProjectFromTemplate } from '../src/utils/filesystem';
 import * as fse from 'fs-extra';
 
 chaiUse(chaiAsPromised);
+
+function pathMatches(expectedPath: string): sinon.SinonMatch {
+	return sinon.match((arg: string) => {
+		if (process.platform === 'win32') { // windows fix
+			arg = arg.replace('/', '\\');
+		}
+		return arg.endsWith(expectedPath);
+	}, "Path not as expected");
+}
 
 suite('Test cases for the filesystem utility functions', () => {
 	suite('Testing createProjectFromTemplate', () => {
@@ -16,8 +26,8 @@ suite('Test cases for the filesystem utility functions', () => {
 		beforeEach(() => sinon.restore());
 
 		test('When checking with an existing project folder, the promise should be rejected', async () => {
-			// @ts-ignore given the project path already exists
-			sinon.stub(fse, 'pathExists').withArgs(dummyProjectPath).resolves(true);
+			// given the project path already exists
+			sinon.stub(fse, 'pathExists').withArgs(pathMatches(dummyProjectPath)).resolves(true);
 			// then the promise should be rejected
 			return expect(createProjectFromTemplate(dummyWorkspaceFolder, dummyProjectName, 'Git'))
 				.to.be.eventually.rejectedWith(
@@ -26,10 +36,10 @@ suite('Test cases for the filesystem utility functions', () => {
 		});
 
 		test('When failed to create the project folder, the promise should be rejected', async () => {
-			// @ts-ignore given the project path doesn't already exists
-			sinon.stub(fse, 'pathExists').withArgs(dummyProjectPath).resolves(false);
+			// given the project path doesn't already exists
+			sinon.stub(fse, 'pathExists').withArgs(pathMatches(dummyProjectPath)).resolves(false);
 			// given the path creation will be rejected with an error
-			sinon.stub(fse, 'ensureDir').withArgs(dummyProjectPath).rejects('the answer is not 41');
+			sinon.stub(fse, 'ensureDir').withArgs(pathMatches(dummyProjectPath)).rejects('the answer is not 41');
 			// then the promise should be rejected
 			return expect(createProjectFromTemplate(dummyWorkspaceFolder, dummyProjectName, 'Git'))
 				.to.be.eventually.rejectedWith(
@@ -38,12 +48,12 @@ suite('Test cases for the filesystem utility functions', () => {
 		});
 
 		test('When failed to copy the template files to the project folder, the promise should be rejected', async () => {
-			// @ts-ignore given the project path doesn't already exists
-			sinon.stub(fse, 'pathExists').withArgs(dummyProjectPath).resolves(false);
+			// given the project path doesn't already exists
+			sinon.stub(fse, 'pathExists').withArgs(pathMatches(dummyProjectPath)).resolves(false);
 			// given the path creation will be resolved
-			sinon.stub(fse, 'ensureDir').withArgs(dummyProjectPath).resolves();
-			// @ts-ignore given the copy process will be rejected with an error
-			sinon.stub(fse, 'copy').withArgs(sinon.match.string ,dummyProjectPath).rejects('it is also not 43');
+			sinon.stub(fse, 'ensureDir').withArgs(pathMatches(dummyProjectPath)).resolves();
+			// given the copy process will be rejected with an error
+			sinon.stub(fse, 'copy').withArgs(sinon.match.string ,pathMatches(dummyProjectPath)).rejects('it is also not 43');
 			// then the promise should be rejected
 			return expect(createProjectFromTemplate(dummyWorkspaceFolder, dummyProjectName, 'Git'))
 				.to.be.eventually.rejectedWith(
@@ -52,12 +62,12 @@ suite('Test cases for the filesystem utility functions', () => {
 		});
 
 		test('When the template files are copied to the project folder successfully, the promise should be resolved', async () => {
-			// @ts-ignore given the project path doesn't already exists
-			sinon.stub(fse, 'pathExists').withArgs(dummyProjectPath).resolves(false);
+			// given the project path doesn't already exists
+			sinon.stub(fse, 'pathExists').withArgs(pathMatches(dummyProjectPath)).resolves(false);
 			// given the path creation will be resolved
-			sinon.stub(fse, 'ensureDir').withArgs(dummyProjectPath).resolves();
-			// @ts-ignore given the copy process will be resolved
-			sinon.stub(fse, 'copy').withArgs(sinon.match.string ,dummyProjectPath).resolves();
+			sinon.stub(fse, 'ensureDir').withArgs(pathMatches(dummyProjectPath)).resolves();
+			// given the copy process will be resolved
+			sinon.stub(fse, 'copy').withArgs(sinon.match.string ,pathMatches(dummyProjectPath)).resolves();
 			// then the promise should be rejected
 			return expect(createProjectFromTemplate(dummyWorkspaceFolder, dummyProjectName, 'Git'))
 				.to.eventually.be.equal(`OCM extension, project ${dummyProjectName} created`);
